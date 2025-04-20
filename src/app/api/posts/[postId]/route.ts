17df348c16fb0125f94d8987a1a1c87fe4c3ad2d
vlad-blip <ezio2506@gmail.com>
2025-04-20 09:00:39 +0300

    + added error handling for prisma errors

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -1,61 +1,94 @@
-import { prisma } from '../../../../../prisma'
-import { postSchema } from '@/lib/schemas/posts';
+import { prisma } from "../../../../../prisma";
+import { postSchema } from "@/lib/schemas/posts";
+import { Prisma } from "@prisma/client";
+import { prismaError } from "@/lib/prisma/error";
 
 export async function GET(
-    request: Request,
-    { params }: { params: Promise<{ postId: string }> }
+  request: Request,
+  { params }: { params: Promise<{ postId: string }> }
 ) {
+  try {
     const { postId } = await params;
 
     const post = await prisma.post.findFirst({
-        where: {
-            id: postId
-        }
-    })
+      where: {
+        id: postId,
+      },
+    });
 
-    return Response.json(post)
+    return Response.json(post);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return prismaError(e);
+    }
+
+    throw e;
+  }
 }
 
 export async function PUT(
-    request: Request,
-    { params }: { params: Promise<{ postId: string }> }
+  request: Request,
+  { params }: { params: Promise<{ postId: string }> }
 ) {
+  try {
     const { postId } = await params;
 
     const res = await request.json();
 
-    const { success, error, data } = postSchema.safeParse(res)
+    const { success, error, data } = postSchema.safeParse(res);
 
     if (!success) {
-        return Response.json(
-            {
-                error: "Validation failed",
-                issues: error.errors,
-            },
-            { status: 400 },
-        )
+      return Response.json(
+        {
+          error: "Validation failed",
+          issues: error.errors,
+        },
+        { status: 400 }
+      );
     }
 
     const post = await prisma.post.update({
-        where: {
-            id: postId
-        }, data
-    })
+      where: {
+        id: postId,
+      },
+      data,
+    });
 
-    return Response.json(post)
+    return Response.json(post);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return prismaError(e);
+    }
+
+    throw e;
+  }
 }
 
 export async function DELETE(
-    request: Request,
-    { params }: { params: Promise<{ postId: string }> }
+  request: Request,
+  { params }: { params: Promise<{ postId: string }> }
 ) {
+  try {
     const { postId } = await params;
 
     const post = await prisma.post.delete({
-        where: {
-            id: postId
-        }
-    })
+      where: {
+        id: postId,
+      },
+    });
 
-    return Response.json(post)
+    return Response.json(post);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return prismaError(e);
+    }
+
+    throw e;
+  }
 }
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,30 +1,52 @@
-import { prisma } from '../../../../prisma'
-import { postSchema } from '@/lib/schemas/posts';
+import { prisma } from "../../../../prisma";
+import { postSchema } from "@/lib/schemas/posts";
+import { Prisma } from "@prisma/client";
+import { prismaError } from "@/lib/prisma/error";
 
 export async function GET() {
+  try {
     const posts = await prisma.post.findMany();
 
-    return Response.json(posts)
+    return Response.json(posts);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return prismaError(e);
+    }
+
+    throw e;
+  }
 }
 
 export async function POST(request: Request) {
+  try {
     const res = await request.json();
 
-    const { success, error, data } = postSchema.safeParse(res)
+    const { success, error, data } = postSchema.safeParse(res);
 
     if (!success) {
-        return Response.json(
-            {
-                error: "Validation failed",
-                issues: error.errors,
-            },
-            { status: 400 },
-        )
+      return Response.json(
+        {
+          error: "Validation failed",
+          issues: error.errors,
+        },
+        { status: 400 }
+      );
     }
 
     const post = await prisma.post.create({
-        data
-    })
+      data,
+    });
 
-    return Response.json(post)
+    return Response.json(post);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return prismaError(e);
+    }
+
+    throw e;
+  }
 }
