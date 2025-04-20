6352ff66d0322508665267ffc5a78f863e64b5d7
vlad-blip <ezio2506@gmail.com>
2025-04-20 08:39:37 +0300

    + added auth protection for api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,34 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+const protectedResources = [
+  {
+    methods: ["DELETE", "PUT", "POST"],
+    pathname: "/api/posts",
+  },
+];
+
+export default auth((req) => {
+  const protectedResource = protectedResources.find(
+    (resource) =>
+      resource.methods.includes(req.method) &&
+      req.nextUrl.pathname.startsWith(resource.pathname)
+  );
+
+  if (!protectedResource) {
+    return NextResponse.next();
+  }
+
+  if (!req.auth) {
+    return NextResponse.json(
+      {},
+      {
+        status: 401,
+      }
+    );
+  }
+});
 
 export const config = {
-    matcher: '/admin/:path*'
-}
+  matcher: ["/admin/:path*", "/api/posts", "/api/posts/:path*"],
+};
